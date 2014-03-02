df51238cdcc156d0ce36c8708aceb98fe802716d
E. Dunham <dunhame@onid.orst.edu>
2014-03-01 23:56:17 -0800

    Skeleton for scales tab thingy

---
+++ scales.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python2.7
+
+import argparse
+
+"""
+Learning music theory by automating the process of figuring out which notes go
+in which scales. Print tab for normally-tuned guitar.
+"""
+
+class Tab(Object):
+    def __init__(self):
+        pass
+    def print(self):
+        pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
