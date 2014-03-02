faf04859e8a7b194cb502be49c17f8b4ff39edf9
E. Dunham <dunhame@onid.orst.edu>
2014-03-02 09:39:49 -0800

    Trying to make scales.
    Intervals from http://library.thinkquest.org/15413/theory/intervals.htm

--- scales.py
+++ scales.py
@@ -7,14 +7,32 @@ Learning music theory by automating the process of figuring out which notes go
 in which scales. Print tab for normally-tuned guitar.
 """
 
-class Tab(Object):
+class Tab():
     def __init__(self):
         pass
-    def print(self):
+    def pprint(self):
         pass
 
+def get_freqs(fn = 'a440'):
+    fd = open(fn, 'r')
+    # each lines is note name, frequency in Hz, wavelength in cm
+    lines = [l.split() for l in fd.readlines()]
+    lines = [[l[0], float(l[1]), float(l[2])] for l in lines]
+    fd.close()
+
+def scale(f, note, type='major'):
+    scales = {'major':[2, 2, 1, 2, 2, 2, 1],
+              'natural minor':[2, 1, 2, 2, 1, 2, 2],
+              'melodic minor':[2, 1, 2, 2, 2, 2, 1], # nat. minor when descending
+              'harmonic minor':[2, 1, 2, 2, 1, 3, 1],
+              'chromatic':[1,1,1,1,1,1,1,1,1,1,1,1,1],
+              }
+              # pentatonic is all strange though
+    
+
 def main():
-    pass
+    f = get_freqs()
+    strings = ['E4', 'B3', 'G3', 'D3', 'A2', 'E2']    
 
 if __name__ == "__main__":
     main()
