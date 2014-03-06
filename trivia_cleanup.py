66b76ead13ccb5470c39676f655fd7c434d99a85
E. Dunham <dunhame@onid.orst.edu>
2014-03-05 18:36:11 -0800

    prune dead code

--- trivia_cleanup.py
+++ trivia_cleanup.py
@@ -19,35 +19,12 @@ def dedup_lines(pathtofile):
     for l in old.readlines():
         m = l.lower().translate(None, ":,;!@#$%^&*()[]?. /\#-") # munge string
 
-        # It'd be nice to find the better-punctuated string and write it, but
-        # that's hard to handle edge cases of n "matching" strings and picking
-        # the best of them.
-
-
         if last == m: 
             group.append(l)
-            print "found dup " + l
         else:
             write_best(group, new)
             group = [l]
             last = m
-
-        """
-        if last == m:
-            group.append(l)
-            continue
-        if len(group) > 0: # just finished a group of matchies
-            new.write(pick_best(group))
-            group = []
-        else:
-            new.write(l)
-        if last != m:
-            new.write(l)
-        else:
-            print "found duplicate: " + l
-        last = m
-        """
-
     old.close()
     new.close()
     bash = 'rm ' + pathtofile + '; mv ' + pathtofile + '-new ' + pathtofile
