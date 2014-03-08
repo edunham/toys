9b8794a845b3157be5ffa99e34a64a296fcf7af8
E. Dunham <dunhame@onid.orst.edu>
2014-03-07 20:19:39 -0800

    start script to find projects in need of attention

---
+++ unloved.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python2.7
+
+"""
+Find unloved projects.
+
+I have ~/code/ on my system, which is full of git repositories. I'd like to
+list those projects and: 
+    * Complain about those which aren't initialized as git repos
+    * Sort by most distant date of latest commit
+    * Complain about the ones lacking licenses  
+"""
+
+
