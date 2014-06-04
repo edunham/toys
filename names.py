e7e7f49f3b63901348ea8f92d4e213e3be3f34f5
E. Dunham <dunhame@onid.orst.edu>
2014-06-03 22:31:21 -0700

    need a thing for naming things.

---
+++ names.py
@@ -0,0 +1,15 @@
+"""
+Names: 
+    Must contain at least one vowel
+    Must contain at least one consonant
+    Capitalization doesn't matter
+    Every letter must follow a letter that it follows somewhere in the English
+    language (so like a Markov chain without probabilities)
+    Names must not occur in a dictionary, a dictionary of misspellings, or a
+    dump of urbandictionary
+    Googling for precisely the name must yield no results (maybe)
+    Shorter is better
+    Generate all up to n-chars to be searchable for substrings (py, mt, etc.)
+"""
+
+
