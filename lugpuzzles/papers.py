c67d38402f6a9ca29386526c137997f2348463b4
edunham <edunham@osuosl.org>
2015-02-08 09:59:19 -0800

    snarky but accurate

---
+++ lugpuzzles/papers.py
@@ -0,0 +1,16 @@
+"""
+After first cutting there's A2, A3, A4, and A5. 
+
+Expected # of times there's ONLY ONE SHEET LEFT
+
+max # of times would be if all but A2 got used, then all but A3 got used, then
+all but A4. 
+
+Min # of times is if he always draws the largest piece, so only once will
+there be a single sheet, when it's the last. 
+"""
+
+print """I expect that the printer always grabs the largest page left, because
+it is the first one his hand comes to when he reaches into the envelope. Thus,
+the number of times which he is expected by me to find only a single sheet is
+1.000000, which is when he removes the final remaining A5 sheet."""
