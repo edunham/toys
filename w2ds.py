766ccd639dd142e7fc882484f4aa02c91d1c3411
edunham <edunham@mozilla.com>
2016-02-29 10:32:48 -0800

    A thing for words with numbers in them

---
+++ w2ds.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+
+def condense(w):
+    print w[0] + str(len(w)-2) + w[-1:]
+
+def expand(w):
+    length = int(w[1:-1]) + 2
+    for word in get_words(length):
+        if word.startswith(w[0]) and word.endswith(w[-1:]):
+            print word
+
+
+def get_words(length, filename = '/usr/share/dict/words'):
+  return (word.strip() for word in open(filename) if len(word) == length)
+
+if __name__ == "__main__":
+    print "Words With Numbers In Them Thing"
+    while(True):
+        w = raw_input("Word: ")
+        try:
+            print "Condensed: "
+            condense(w)
+        except:
+            pass
+        try:
+            print "Expanded: "
+            expand(w)
+        except:
+            print "Could not expand " + w
