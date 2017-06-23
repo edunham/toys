d182acdd682177cad22816edad46155c921ca53b
E. Dunham <github@edunham.net>
2017-06-23 14:51:56 -0700

    run this in the servo repo to count merges per month

---
+++ utilities/merges-per-month.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+begin=$(date -I -d "2012-02-01")
+end=$(date -I -d "2017-07-01") # should automatically be first day of next month
+
+while [[ "$begin" < "$end" ]]; do
+    temp=$(date -I -d "$begin + 1 month")
+    n=$(git log --author="bors-servo" --pretty=format:"%cd  %h  %s" --before={$temp} --after={$begin} | wc -l)
+    echo $n from $begin to $temp
+    begin=$temp
+done
